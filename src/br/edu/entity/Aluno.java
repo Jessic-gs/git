94e0ec51c0ab08699ccbac2ee4367b311d32cb88
Felipe Willian Torres de Camargo <felipetorresdecamargo@hotmail.com>
2015-04-29 18:33:03 -0300

    Segundo commit...

--- src/br/edu/entity/Aluno.java
+++ src/br/edu/entity/Aluno.java
@@ -6,5 +6,30 @@ public class Aluno {
 	private String nome;
 	private String telefone;
 	private String email;
+	
+	public int getRa() {
+		return ra;
+	}
+	public void setRa(int ra) {
+		this.ra = ra;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
